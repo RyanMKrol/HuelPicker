67689a7ed2b3cbfaa74d72627d3dce2460f3bfad
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-18 14:26:24 +0100

    Add flavour chooser

---
+++ src/app.js
@@ -0,0 +1,51 @@
+import schedule from 'node-schedule';
+import MailSender from 'noodle-email';
+
+import gmailCredentials from '../credentials/GmailCredentials.json';
+
+const mailClient = new MailSender(gmailCredentials);
+
+mailClient.setFrom('"HuelPicker" <ryankrol.m@gmail.com>');
+mailClient.setTo('ryankrol.m@gmail.com');
+
+const FLAVOURS = [
+  'Salted Caramel',
+  'Strawberry',
+  'Banana',
+  'Apple Cinnamon',
+  'Chocolate',
+  'Peanut Butter',
+  'Gingerbread',
+  'Chocolate Cherry',
+  'Mint-Chocolate',
+  'Berry',
+  'Mocha',
+  'Pumpkin Spice',
+  'Vanilla',
+  'Rhubarb and Custard',
+  'Toffee',
+];
+
+function randomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function generateIndicesFromArray(array) {
+  const indicesSet = new Set();
+
+  while (indicesSet.size < 5) {
+    indicesSet.add(randomNumber(array.length));
+  }
+
+  return indicesSet;
+}
+
+schedule.scheduleJob('0 0 8 * * *', async () => {
+  try {
+    const indicesArray = Array.from(generateIndicesFromArray(FLAVOURS));
+    const flavours = indicesArray.map((index) => FLAVOURS[index]);
+    mailClient.sendMail('Daily Huel Flavours!', JSON.stringify(flavours));
+  } catch (error) {
+    mailClient.sendMail('Failed to pick Huel flavours!', error.toString());
+  }
+});
